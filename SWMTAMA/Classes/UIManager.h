e3816338e56e30334fb81e7311493766b918e029
Plulena <frf1226@gmail.com>
2012-11-08 18:27:20 +0900

    Revert "Question Icon is added"
    This reverts commit 1102eb051c786f15a240d90f6198f7ec013d1afa.

--- SWMTAMA/Classes/UIManager.h
+++ SWMTAMA/Classes/UIManager.h
@@ -24,8 +24,7 @@ enum UI_INGAME { INGAME_START = 10000,
 	TRAIN_NAME,
 	TRAIN_LEVEL, 
 	TEXTBTN_EAT, 
-	TEXTBTN_NAME,
-    ICON_QUESTION, INGAME_END };
+	TEXTBTN_NAME, INGAME_END };
 
 class UIManager
 {
@@ -39,8 +38,7 @@ public:
 	void setDataManager(DataManager* pDataManager);
 	void frame(cocos2d::CCLayer* pLayer, float dt);
 	void setSpeaker(cocos2d::CCLayer* pLayer, UI_INGAME speaker_only);
-    void setQuestion(cocos2d::CCLayer* pLayer, bool visible);
-    
+
 public:
 	void TouchesBegan(cocos2d::CCLayer* pLayer, cocos2d::CCSet *pTouches, cocos2d::CCEvent *pEvent);
 	void TouchesMoved(cocos2d::CCLayer* pLayer, cocos2d::CCSet *pTouches, cocos2d::CCEvent *pEvent);
