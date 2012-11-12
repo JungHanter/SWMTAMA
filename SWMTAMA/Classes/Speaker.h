41147535423be35757c141efc12a75d500cf0200
Plulena <frf1226@gmail.com>
2012-11-12 17:35:43 +0900

    delete

--- SWMTAMA/Classes/Speaker.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Speaker
-{
-public:
-	Speaker(void);
-	~Speaker(void);
-};
-
--- SWMTAMA/proj.android/gen/com/swm/vg/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.swm.vg;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
